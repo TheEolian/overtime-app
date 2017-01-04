a2de254226fc0a854f6084fec7d4d778247296b2
theeolian <msoldmann@gmail.com>
2017-01-04 03:11:44 +0000

    Implemented the ability to delete post records

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,22 @@ describe 'navigate' do
      end
   end
   
+  describe 'new' do
+      it 'has a page link from the homepage' do
+        visit root_path
+        
+        click_link("new_post_from_nav")
+        expect(page.status_code).to eq(200)
+  
+  describe 'delete' do
+      it 'can be deleted' do
+          @post = FactoryGirl.create(:post)  
+         visit posts_path 
+         
+         click_link("delete_post_#{@post.id}_from_index")
+        expect(page.status_code).to eq(200)
+      end
+  
   decribe 'creation' do
       before do
           visit new_post_path
