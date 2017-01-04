e1f54105308662dbc1ce7e2c9930d89dce76a4b4
theeolian <msoldmann@gmail.com>
2017-01-04 02:38:47 +0000

    Implemented edit and update functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
     
   before do
-          @user = FactoryGirl.create(:user)
-      end
+      @user = FactoryGirl.create(:user)
+  end
       
   describe "creation" do 
      it "can be created" do
@@ -21,5 +21,7 @@ RSpec.describe User, :type => :model do
   describe "custom name methods" do
       it 'has a full name method that combines first and alst name' do
           expect(@user.full_name).to eq("SNOW, JON")
+      end
   end
 end
+
